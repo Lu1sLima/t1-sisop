e93c069818faa0776e31fe8fbf8c9eeb2d38ca86
luis-lima <luis.contato01@gmail.com>
2022-05-04 00:12:57 -0300

    added execute_processes method

--- os.py
+++ os.py
@@ -1,21 +1,31 @@
-from process import Process, Priority
+from process import Process, State
 from typing import List
 import re
+import sys
 import os
 
 class OS():
     def __init__(self):
         self.acc: float = 0.0
         self.pc: int = None
-        self.escalonator = []
+        self.queues = {
+            "high": [],
+            "medium": [],
+            "low": []
+        }
         self.quantum = 0
+        self.global_time = 0
+
+    def run_process(self, process: Process):
+        # Aqui de fato rodar as instruções dos processos!
+        pass
 
     def __parse_code(self, code_string) -> List[str]:
         code_string = code_string.strip()
         labels = {}
         instructions = []
 
-        for i, code in enumerate(code_string.split('\n')):
+        for code in code_string.split('\n'):
             if ':' in code:
                 label, code = code.split(':')
                 labels[label.strip()] = code.strip()
@@ -34,19 +44,47 @@ class OS():
 
         return data_dict
 
-    def read_process(self, filename: str):
-        file_path = os.path.join('process_files', filename)
-        with open(file_path, 'r') as process_file:
-            content = process_file.read()
-            code = re.search('(?s)(?<=.code).*?(?=.endcode)', content).group()
-            data = re.search('(?s)(?<=.data).*?(?=.enddata)', content).group()
+    def read_processes(self, processes_string):
+        min_time = float('inf')
+        for process in processes_string.split(', '):
+            filename, priority, arrival_time = process.split()
+            arrival_time = int(arrival_time)
+            if arrival_time < min_time:
+                min_time = arrival_time
+            with open(os.path.join('process_files', filename), 'r') as process_file:
+                content = process_file.read()
+                code = re.search('(?s)(?<=.code).*?(?=.endcode)', content).group()
+                data = re.search('(?s)(?<=.data).*?(?=.enddata)', content).group()
 
-            instructions, labels = self.__parse_code(code)
-            data_dict = self.__parse_data(data)
+                instructions, labels = self.__parse_code(code)
+                data_dict = self.__parse_data(data)
 
-            self.escalonator.append(Process(instructions, labels, data_dict, Priority.HIGH))
+                self.queues[priority.lower()].append(Process(instructions, labels, data_dict, State.READY, arrival_time))
+        self.global_time = min_time
+    
+    def exec_processes(self):
+        while True:
+            process = None
+            for priority, queue in self.queues.items():
+                if queue:
+                    process = self.queues[priority][0]
+                    if process.arrival_time <= self.global_time:
+                        process.state = State.RUNNING
+                        break
+                    else:
+                        continue
+                else:
+                    continue
+            
+            ## Contar quantum com o processo corrente
+            for i in range(self.quantum):
+                self.global_time += 1
+                self.run_process(process)
+
 
 if __name__ == "__main__":
     os_ = OS()
+    process = 'p1.txt HIGH 10, p2.txt MEDIUM 2, p2.txt MEDIUM 2'
 
-    os_.read_process('p1.txt')
+    os_.read_processes(process)
+    os_.exec_processes()
--- process.py
+++ process.py
@@ -1,29 +1,19 @@
 from enum import Enum
 
 
-class Priority(Enum):
-    LOW = 0
-    MEDIUM = 0.5
-    HIGH = 1
-
-# [Proc(p=0.5), Proc(p=0)]
-
-# exe : Proc(p=1)
-# quantum: [Proc(p=1), Proc(p=0.5), Proc(p=0)]
-
+class State(Enum):
+    READY = 'Ready'
+    READY_SUSPENDED = 'Ready/Suspended'
+    RUNNING = 'Running'
+    BLOCKED = 'Blocked'
+    BLOCKED_SUSPENDED = 'Blocked/Suspended'
+    EXIT = 'Exit'
 
 class Process():
-    def __init__(self, instructions: list, labels: dict, data: dict, priority: Priority):
+    def __init__(self, instructions: list, labels: dict, data: dict, state: State, arrival_time: int):
         self.instructions = instructions
         self.data = data
         self.labels = labels
-        self.priority = priority.value
-
-    def __eq__(self, other) -> bool:
-        return self.priority == other.priority
-
-    def __lt__(self, other) -> bool:
-        return self.priority < other.priority
-
-    def __gt__(self, other) -> bool:
-        return self.priority > other.priority
+        self.state = state.value
+        self.last_pc = -1
+        self.arrival_time = arrival_time
