3f0f936864f88f0dfd28cfd6b2f886250c51e33b
luis-lima <luis.contato01@gmail.com>
2022-05-07 00:08:54 -0300

    Switched input file format to json and added a print function to see queues while running

--- os.py
+++ os.py
@@ -1,26 +1,31 @@
-from process import Process, State
-from typing import List
 import re
 import sys
+import time
 import os
+import json
+
+from process import Process, State
+from typing import List
 
 class OS():
     def __init__(self):
         self.acc: float = 0.0
         self.pc: int = None
         self.queues = {
-            "high": [],
-            "medium": [],
-            "low": []
+            "HIGH": [],
+            "MEDIUM": [],
+            "LOW": []
         }
         self.quantum = 2
         self.global_time = 0
+        self.memory_capacity = 0
+        self.p_list = []
 
     def run_process(self, process: Process):
         # Aqui de fato rodar as instruções dos processos!
         pass
 
-    def __parse_code(self, code_string) -> List[str]:
+    def __parse_code(self, code_string: str) -> List[str]:
         code_string = code_string.strip()
         labels = {}
         instructions = []
@@ -34,7 +39,7 @@ class OS():
 
         return instructions, labels
 
-    def __parse_data(self, data_string):
+    def __parse_data(self, data_string: str) -> dict:
         data_string = data_string.strip()
         data_dict = {}
 
@@ -44,14 +49,22 @@ class OS():
 
         return data_dict
 
-    def read_processes(self, processes_string):
+    def read_input_file(self, filename: str) -> None:
+        with open(filename, 'r') as file:
+            file_json = json.loads(file.read())
+            self.quantum = file_json['quantum']
+            self.memory_capacity = file_json['memory']
+
+            self.__read_processes(file_json['processes'])
+
+    def __read_processes(self, processes: dict) -> None:
         min_time = float('inf')
-        for process in processes_string.split(', '):
-            filename, priority, arrival_time = process.split()
-            arrival_time = int(arrival_time)
+        for i, process_info in enumerate(processes):
+            arrival_time = int(process_info['arrival_time'])
             if arrival_time < min_time:
                 min_time = arrival_time
-            with open(os.path.join('process_files', filename), 'r') as process_file:
+            with open(os.path.join(process_info['filename']), 'r') as process_file:
+                priority = process_info['priority'].upper()
                 content = process_file.read()
                 code = re.search('(?s)(?<=.code).*?(?=.endcode)', content).group()
                 data = re.search('(?s)(?<=.data).*?(?=.enddata)', content).group()
@@ -59,11 +72,37 @@ class OS():
                 instructions, labels = self.__parse_code(code)
                 data_dict = self.__parse_data(data)
 
-                self.queues[priority.lower()].append(Process(instructions, labels, data_dict, State.READY, arrival_time))
+                process = Process(i, priority, instructions, labels, data_dict, State.READY, arrival_time)
+                self.queues[priority].append(process)
+                self.p_list.append(process)
         self.global_time = min_time
     
+    def __do_state_change(process, state):
+        pass
+
+
+    def __print_queues(self):
+        time.sleep(0.5)
+        os.system('cls' if os.name == 'nt' else 'clear')
+        biggest = 0
+        for state in State:
+            process_list_str = ""
+            processes_in_state = filter(lambda p : p.state == state, self.p_list)
+            for process in processes_in_state:
+                process_list_str += f"PID {process.pid} ({process.priority}), "
+            print_str = f"| {state.value} Queue: [{process_list_str[:-2]}]"
+            space_plus = " " * (biggest - len(print_str)-1)
+            print_str += f"{space_plus}|"
+            if len(print_str) > biggest:
+                biggest = len(print_str)
+            line = "-" * biggest
+            print(line)
+            print(print_str)
+        print(line)
+
     def exec_processes(self):
         while True:
+            self.__print_queues()
             process = None
             for priority, queue in self.queues.items():
                 if queue:
@@ -88,13 +127,14 @@ class OS():
                         pass
                     self.run_process(process)
                     self.global_time += 1
+                # process.state = State.
             else:
                 self.global_time += 1
 
 
 if __name__ == "__main__":
     os_ = OS()
-    process = 'p1.txt HIGH 10, p2.txt MEDIUM 2'
+    process = 'input.json'
 
-    os_.read_processes(process)
+    os_.read_input_file(process)
     os_.exec_processes()
--- process.py
+++ process.py
@@ -10,10 +10,18 @@ class State(Enum):
     EXIT = 'Exit'
 
 class Process():
-    def __init__(self, instructions: list, labels: dict, data: dict, state: State, arrival_time: int):
+    def __init__(self, pid: int, priority: str, instructions: list, labels: dict, data: dict, state: State, arrival_time: int):
+        self.pid = pid
         self.instructions = instructions
         self.data = data
         self.labels = labels
-        self.state = state.value
+        self.state = state
         self.last_pc = -1
+        self.priority = priority
         self.arrival_time = arrival_time
+
+    def __eq__(self, other):
+        return self.pid == other.pid
+
+    def __str__(self):
+        return f"PID {self.pid} ({self.priority})"
